d70b8673f605e10ddfca67710214d697779bd55c
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-06-10 11:22:40 +0800

    can retrieve and save logs

--- index.js
+++ index.js
@@ -25,94 +25,102 @@ db.once("open", function () {
 
 const Schema = mongoose.Schema;
 
+const exerciseSchema = new Schema({
+        description: {
+          type: String,
+          required: true,
+        },
+        duration: {
+          type: Number,
+          required: true,
+        },
+        date: {
+          type: String
+          
+        },
+      
+})
 
-
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  log: [
-    {
-      description: {
-        type: String,
-        required: true,
-      },
-      duration: {
-        type: Number,
-        required: true,
-      },
-      date: {
-        type: Date,
-        required: true,
-      },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
     },
-  ],
-});
+    log: [exerciseSchema],
+  },
+  { versionKey: false }
+);
 
 const User = mongoose.model("User", userSchema);
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
  app.use(cors());
 
 const path = require("path");
+const { request } = require("http");
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-  res.json({ message: "working" });
+ app.use(express.static(path.join(__dirname, "/public")));
 });
 
 app.post("/api/users",async (req, res) => {
-  const username = req.body.username;
-  if (!username) {
-   console.log("username's request not found")
-  }
-  else {
-    let user = new User({ username, log :[] });
+ 
+    let user = new User({ username : req.body.username });
     await user.save()
-    .then((result) => {res.json({result})})
+    .then((result) => {res.json({ username: result.username, _id: result._id });})
     .catch((error) => console.log(error))
-  }})
+  })
 
   app.get("/api/users", async (req, res) => {
     User.find()
     .then((result) => {
-      res.json(result)
+      res.json(
+        result.map((item) => ({
+          username: item.username,
+          _id: item._id,
+        }))
+      );
     })
     .catch((error) => console.log(error))
   })
 
   app.post("/api/users/:_id/exercises", async (req, res) => {
         const id = req.params._id;
-         const {description,duration}  = req.body;
-  
-        User.findById({_id : id})
+         const {description,duration, date}  = req.body;
+         console.log(date, typeof date);
+          if (date === "" || date === "Invalid Date") {
+            date = new Date().toISOString().substring(0, 10);
+          }
+         let exercise =  new Exercise({
+           description : description,
+           duration : parseInt(duration),
+           date : date
+         })
+         exercise.save();
+        User.findByIdAndUpdate( {_id : id}, 
+          {$push: {log :exercise}}
+         )
         .then(async (result) => {
-           const date = new Date();
+          const logs = await result.log.map((log) => {
+            return {
+              description: log.description,
+              duration: log.duration,
+              date: new Date(log.date).toDateString(),
+            };
+          })
           
-       
-          await User.updateOne({_id: result._id}, {
-            log : {
-              description,
-              duration,
-              date: date
-            }
-           });
-           
-            res.json({
-              username: result.username,
-              count: result.log.length,
-              _id: result._id,
-              log: [
-                {
-                  description,
-                  duration,
-                  date,
-                },
-              ],
-            });
-        })
+           res.json({
+             username: result.username,
+             count: result.log.length,
+             _id : result._id,
+             log: logs
+           });   
+            })
         .catch((error) => console.log(error))
        
   });
