0bf517f88a4c791320f2264d8ac046d2dfd2a95c
IssaDia <44264590+IssaDia@users.noreply.github.com>
2022-06-10 12:57:34 +0800

    can retrieve user, exercise and log data

--- index.js
+++ index.js
@@ -26,19 +26,27 @@ db.once("open", function () {
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
+        username : String,
         description: {
           type: String,
-          required: true,
-        },
-        duration: {
-          type: Number,
           required: true,
         },
         date: {
           type: String
           
         },
-      
+        duration: {
+          type: Number,
+          required: true,
+        },
+}, { versionKey: false }
+);
+
+
+const logSchema = new Schema({
+  username : String,
+  count: Number,
+  log : []
 })
 
 const userSchema = new Schema(
@@ -47,13 +55,15 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    log: [exerciseSchema],
+    // log: [exerciseSchema],
   },
   { versionKey: false }
 );
 
 const User = mongoose.model("User", userSchema);
 const Exercise = mongoose.model("Exercise", exerciseSchema);
+const Log = mongoose.model("Log", logSchema );
+
 
 
 app.use(bodyParser.json());
@@ -91,38 +101,76 @@ app.post("/api/users",async (req, res) => {
 
   app.post("/api/users/:_id/exercises", async (req, res) => {
         const id = req.params._id;
+
          const {description,duration, date}  = req.body;
-         console.log(date, typeof date);
+
+        
           if (date === "" || date === "Invalid Date") {
             date = new Date().toISOString().substring(0, 10);
           }
          let exercise =  new Exercise({
            description : description,
            duration : parseInt(duration),
-           date : date
+           date : date,
+           _id : id
          })
          exercise.save();
+
         User.findByIdAndUpdate( {_id : id}, 
           {$push: {log :exercise}}
          )
         .then(async (result) => {
-          const logs = await result.log.map((log) => {
-            return {
-              description: log.description,
-              duration: log.duration,
-              date: new Date(log.date).toDateString(),
-            };
-          })
+          Exercise.findOne({ _id: id }).then((data) => {
+            console.log("data", data, "result", result);
+            res.json({
+              _id: result._id,
+              username: result.username,
+              date: data.date,
+              duration: data.duration,
+              description: data.description,
+            });
+          });
+          // const logs = await result.log.map((log) => {
+          //   return {
+          //     description: log.description,
+          //     duration: log.duration,
+          //     date: new Date(log.date).toDateString(),
+          //   };
+          // })
           
-           res.json({
-             username: result.username,
-             count: result.log.length,
-             _id : result._id,
-             log: logs
-           });   
+          //  res.json({
+          //    username: result.username,
+          //    count: result.log.length,
+          //    _id : result._id,
+          //    log: exercise
+          //  });   
             })
         .catch((error) => console.log(error))
        
+  });
+
+  app.get("/api/users/:_id/logs", async (req, res) => {
+    const id = req.params._id;
+   
+    User.findOne({ _id: id })
+      .then(async (result) => {
+        const logs = await result.log.map((log) => {
+          return {
+            description: log.description,
+            duration: log.duration,
+            date: new Date(log.date).toDateString(),
+          };
+        });
+        
+        res.json({
+          username: result.username,
+          count: result.log.length,
+          _id: result._id,
+         log: logs,
+        });
+       
+      })
+      .catch((error) => console.log(error));
   });
 
   app.get("/api/users/:id/logs", (req, res) => {
